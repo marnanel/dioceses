4069146492a8606f7c34764d8c7a6840fc08cbf0
Thomas Thurman <thomas@thurman.org.uk>
2011-03-12 16:27:08 -0500

    FIXME?

--- src/combine-all.pl
+++ src/combine-all.pl
@@ -3,5 +3,6 @@ use warnings;
 
 for my $file (glob('temp/*.svg')) {
 	print "$file\n";
+	# XXX FIXME: I think this should be SelectionUnion
 	system("inkscape $file --verb EditSelectAll --verb SelectionCombine --verb FileSave --verb FileClose");
 }
